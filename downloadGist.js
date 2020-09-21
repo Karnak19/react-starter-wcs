f6a46136a8960d40434c4f3060fc1b899091d87e
Karnak19 <basile64.v@gmail.com>
2020-09-21 11:35:49 +0200

    add ESLint for better code style

--- downloadGist.js
+++ downloadGist.js
@@ -1,18 +1,22 @@
-const axios = require("axios");
+const axios = require('axios');
 
 async function downloadGist(url, ids) {
   try {
     const res = await axios.get(url, {
       headers: {
-        "User-Agent": "curl/7.30.0",
-        Host: "api.github.com",
-        Accept: "*/*",
+        'User-Agent': 'curl/7.30.0',
+        Host: 'api.github.com',
+        Accept: '*/*',
       },
     });
 
-    return res.data.filter((gist) => ids.some((id) => id === gist.id)).map((e) => e.files);
+    return res.data
+      .filter((gist) => ids.some((id) => id === gist.id))
+      .map((e) => e.files);
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error);
+    return error;
   }
 }
 
--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 #! /usr/bin/env node
 
-const fs = require("fs");
-const clear = require("clear");
-const ora = require("ora");
-const axios = require("axios");
-const mkdirp = require("mkdirp");
-const chalk = require("chalk");
+const fs = require('fs');
+const clear = require('clear');
+const ora = require('ora');
+const axios = require('axios');
+const mkdirp = require('mkdirp');
+const chalk = require('chalk');
 
-const runCommand = require("./runCommand");
+const runCommand = require('./runCommand');
 const {
   promptHeader,
   promptEnd,
   depsPrompt,
   caproverPrompt,
   ghActionsPrompt,
-} = require("./prompts");
-const downloadGist = require("./downloadGist");
-const dependencies = require("./deps.json");
+} = require('./prompts');
+const downloadGist = require('./downloadGist');
+const dependencies = require('./deps.json');
 
-const repoURL = "https://github.com/Karnak19/react-starter-template.git";
+const repoURL = 'https://github.com/Karnak19/react-starter-template.git';
 const name = process.argv[2];
 
 clear();
 
+// eslint-disable-next-line no-control-regex
 if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  // eslint-disable-next-line no-console
   return console.log(`
-  ${chalk.red("Invalid directory name.")}
+  ${chalk.red('Invalid directory name.')}
   Usage: react-starter-wcs my-app  
 `);
 }
@@ -42,61 +44,61 @@ let spinner;
     const ghActions = await ghActionsPrompt.run();
 
     const datas = {
-      deps: deps.flatMap((dep) => {
-        return dependencies[dep].packages ? dependencies[dep].packages : null;
-      }),
-      caprover: caprover[0] ? true : false,
+      deps: deps.flatMap((dep) =>
+        dependencies[dep].packages ? dependencies[dep].packages : null,
+      ),
+      caprover: !!caprover[0],
       ghActions:
         ghActions.length > 0
           ? ghActions.map((e) => {
-              if (e === "ESLint on PR") {
-                return "eslint";
+              if (e === 'ESLint on PR') {
+                return 'eslint';
               }
               if (e === "Build and push to a 'production' branch") {
-                return "build";
+                return 'build';
               }
               return e;
             })
           : false,
     };
 
-    await runCommand("git", ["clone", repoURL, name]);
-    await runCommand("rm", ["-rf", `${name}/.git`]);
-    await runCommand("git", ["init", `${name}`]);
+    await runCommand('git', ['clone', repoURL, name]);
+    await runCommand('rm', ['-rf', `${name}/.git`]);
+    await runCommand('git', ['init', `${name}`]);
 
-    spinner = ora("Installing dependencies...").start();
+    spinner = ora('Installing dependencies...').start();
 
-    await runCommand("npm", ["install"], {
-      cwd: process.cwd() + "/" + name,
+    await runCommand('npm', ['install'], {
+      cwd: `${process.cwd()}/${name}`,
     });
 
     if (datas.deps.length > 0) {
-      await runCommand("npm", ["install", ...datas.deps], {
-        cwd: process.cwd() + "/" + name,
+      await runCommand('npm', ['install', ...datas.deps], {
+        cwd: `${process.cwd()}/${name}`,
       });
     }
 
     if (datas.caprover) {
       // Download Caprover files gist
-      const gists = await downloadGist("https://api.github.com/users/Karnak19/gists", [
-        dependencies.caprover.id,
-      ]);
+      const gists = await downloadGist(
+        'https://api.github.com/users/Karnak19/gists',
+        [dependencies.caprover.id],
+      );
 
-      const files = gists.flatMap((gist) => {
-        return Object.values(gist).map((val) => {
-          return {
-            promise: axios.get(val.raw_url),
-          };
-        });
-      });
+      const files = gists.flatMap((gist) =>
+        Object.values(gist).map((val) => ({
+          promise: axios.get(val.raw_url),
+        })),
+      );
 
       const res = await Promise.all(files.map((f) => f.promise));
 
       res.forEach((f) => {
-        if (typeof f.data === "object") {
+        if (typeof f.data === 'object') {
+          // eslint-disable-next-line no-param-reassign
           f.data = JSON.stringify(f.data);
         }
-        filename = f.request.path.split("/")[5];
+        const filename = f.request.path.split('/')[5];
         fs.writeFile(`${process.cwd()}/${name}/${filename}`, f.data, (err) => {
           if (err) throw err;
         });
@@ -105,28 +107,32 @@ let spinner;
 
     if (datas.ghActions) {
       // Download GH Actions files gist
-      const ids = datas.ghActions.map((e) => {
-        return dependencies[e].id;
-      });
-      const gists = await downloadGist("https://api.github.com/users/Karnak19/gists", ids);
+      const ids = datas.ghActions.map((e) => dependencies[e].id);
+      const gists = await downloadGist(
+        'https://api.github.com/users/Karnak19/gists',
+        ids,
+      );
 
-      const files = gists.flatMap((gist) => {
-        return Object.values(gist).map((val) => {
-          return {
-            promise: axios.get(val.raw_url),
-          };
-        });
-      });
+      const files = gists.flatMap((gist) =>
+        Object.values(gist).map((val) => ({
+          promise: axios.get(val.raw_url),
+        })),
+      );
 
       const res = await Promise.all(files.map((f) => f.promise));
 
       await mkdirp(`${process.cwd()}/${name}/.github/workflows`);
 
       res.forEach((f) => {
-        filename = f.request.path.split("/")[5];
-        fs.writeFile(`${process.cwd()}/${name}/.github/workflows/${filename}`, f.data, (err) => {
-          if (err) throw err;
-        });
+        // eslint-disable-next-line prefer-destructuring
+        const filename = f.request.path.split('/')[5];
+        fs.writeFile(
+          `${process.cwd()}/${name}/.github/workflows/${filename}`,
+          f.data,
+          (err) => {
+            if (err) throw err;
+          },
+        );
       });
     }
 
@@ -135,6 +141,7 @@ let spinner;
   } catch (error) {
     // spinner.stop();
     // promptEnd(name);
+    // eslint-disable-next-line no-console
     console.error(error);
   }
 })();
