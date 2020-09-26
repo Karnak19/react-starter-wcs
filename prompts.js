05c2a0d0a513ed6ad0697cdcf0e16f475368730c
Basile Vernouillet <basile64.v@gmail.com>
2020-09-26 22:50:05 +0200

    added some CSS frameworks in the deps prompt

--- prompts.js
+++ prompts.js
@@ -1,24 +1,24 @@
-const chalk = require("chalk");
-const figlet = require("figlet");
-const Prompt = require("prompt-checkbox");
+const chalk = require('chalk');
+const figlet = require('figlet');
+const Prompt = require('prompt-checkbox');
 
 const promptHeader = () =>
   console.log(
     chalk.yellow(
-      figlet.textSync("react-starter-wcs", {
-        horizontalLayout: "fitted",
-      })
-    )
+      figlet.textSync('react-starter-wcs', {
+        horizontalLayout: 'fitted',
+      }),
+    ),
   );
 const promptEnd = (name) => {
-  colorlog("Success ! 🔥", chalk.green);
-  console.log("");
-  console.log("");
-  colorlog("Getting started :", chalk.yellow);
-  console.log("");
+  colorlog('Success ! 🔥', chalk.green);
+  console.log('');
+  console.log('');
+  colorlog('Getting started :', chalk.yellow);
+  console.log('');
   colorlog(`  cd ${name}`, chalk.cyan);
-  colorlog("  npm run dev", chalk.cyan);
-  console.log("");
+  colorlog('  npm run dev', chalk.cyan);
+  console.log('');
 };
 
 function colorlog(text, color) {
@@ -26,22 +26,31 @@ function colorlog(text, color) {
 }
 
 const depsPrompt = new Prompt({
-  name: "packages",
-  message: "Do you want any third-party libraries ? \n(press space to choose, enter to confirm)\n",
-  choices: ["react-router", "redux", "recoil", "prop-types"],
+  name: 'packages',
+  message:
+    'Do you want any third-party libraries ? \n(press space to choose, enter to confirm)\n',
+  choices: [
+    'react-router',
+    'redux',
+    'recoil',
+    'prop-types',
+    'material-ui',
+    'semantic-ui',
+    'reactstrap',
+  ],
 });
 
 const caproverPrompt = new Prompt({
-  name: "caprover",
-  message: "Do you want Caprover config files ?",
+  name: 'caprover',
+  message: 'Do you want Caprover config files ?',
   radio: true,
-  choices: ["yes"],
+  choices: ['yes'],
 });
 
 const ghActionsPrompt = new Prompt({
-  name: "actions",
-  message: "Do you want any pre-configured Github Actions ?",
-  choices: ["ESLint on PR", "Build and push to a 'production' branch"],
+  name: 'actions',
+  message: 'Do you want any pre-configured Github Actions ?',
+  choices: ['ESLint on PR', "Build and push to a 'production' branch"],
 });
 
 module.exports = {
